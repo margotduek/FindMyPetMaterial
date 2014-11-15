e4fa697a83388cfbfdb122b36f45fc676255077d
Ray <a01020121@itesm.mx>
2014-11-14 18:46:02 -0600

    Ya todas las listas funcionan. Se necesita poner botón report.

--- app/src/main/java/com/petfinder/margot/findmypetmaterial/fragmentos/EncontradosLista.java
+++ app/src/main/java/com/petfinder/margot/findmypetmaterial/fragmentos/EncontradosLista.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.petfinder.margot.findmypetmaterial.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Margot on 10/11/2014.
  */
 public class EncontradosLista extends android.support.v4.app.Fragment {
+    ListView listViewPerros;
 
     public static EncontradosLista newInstance(int position) {
 
@@ -27,7 +31,19 @@ public class EncontradosLista extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_encontrados_lista,container,false);
 
+        listViewPerros =(ListView) v.findViewById(R.id.listView);
 
+        //Aqui alimentamos la lista con datos.
+        ArrayList<Perros> listaPerros = new ArrayList<Perros>();
+
+        for (int contador = 0; contador < 7; contador ++){
+            Perros nuevoPerro = new Perros();
+            nuevoPerro.setNombre("Raycillo");
+            nuevoPerro.setCaracteristicas("buena vibra ;)");
+
+            listaPerros.add(nuevoPerro);
+        }
+        this.listViewPerros.setAdapter(new AdapterPerros(getActivity(),listaPerros));
 
 
 
